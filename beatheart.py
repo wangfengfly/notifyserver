a48ce198ef8ec9304bd0207ff35baba20f3a2476
wangfeng211731 <wangfeng211731@sogou-inc.com>
2016-08-16 10:24:40 +0800

    replace zmq send with C implementation

--- beatheart.py
+++ beatheart.py
@@ -3,7 +3,9 @@
 
 import threading
 import time
-from ZmqFactory.ServerFactory import ServerFactory
+import ctypes as ct
+
+#from ZmqFactory.ServerFactory import ServerFactory
 
 class BeatHeart(threading.Thread):
 	def __init__(self, seconds, logger):
@@ -15,5 +17,10 @@ class BeatHeart(threading.Thread):
 	def run(self):
 		while True:
 			msg = "<L><DataFinder><L><" + str(time.time()) + ">"
-			ServerFactory.getServer("push").send(msg)
+                        ll = ct.cdll.LoadLibrary 
+                        lib = ll("/home/soft/msgsend_lib/libmsgsend.so")   
+                        lib.SetConnect("192.168.60.27", "6677")
+                        lib.MsgSend("DataFinder", "Alive")
+                        lib.MsgSend("DataFinder", msg)
+#			ServerFactory.getServer("push").send(msg)
 			time.sleep(self.seconds)
